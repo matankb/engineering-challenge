8ccd078c0efae10712b21f02350e80821a132abc
Matan Kotler-Berkowitz <205matan@gmail.com>
2015-11-25 21:41:05 +0000

    Implement speech recognition callback

--- motor.py
+++ motor.py
@@ -2,14 +2,25 @@ import RPi.GPIO as GPIO
 import speech_recognition as sr
 from Queue import Queue
 
+event_queue = Queue()
+
+rec = sr.Recognizer()
+mic = sr.Microphone()
+
+def phrase_heard(rec, audio):
+  try:
+    event_queue.put(rec.recognize_google(audio))
+  except sr.UnknownValueError:
+    print('Error: could not recognize speech')
+  except sr.RequestError as e:
+    print('Error: could not complete request; {0}'.format(e))
+
 #set mode for identifying pins
 GPIO.setmode(GPIO.BOARD)
 
 #set constants for GPIO pin numbers
 MOTOR_RIGHT = 16
 MOTOR_LEFT = 18
-
-event_queue = Queue()
 
 while True:
   command = event_queue.get()
