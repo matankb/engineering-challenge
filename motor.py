d252e3cb25e65c2660974eda52a255151d9efe8f
Matan Kotler-Berkowitz <205matan@gmail.com>
2015-11-25 11:18:57 -0500

    Update motor.py

--- motor.py
+++ motor.py
@@ -7,3 +7,16 @@ GPIO.setmode(GPIO.BOARD)
 #set constants for GPIO pin numbers
 MOTOR_RIGHT = 11
 MOTOR_LEFT = 12
+
+while True:
+  input = input('Direction:') #Will be subsituted for voice control
+  if input == 'left':
+    GPIO.output(MOTOR_LEFT, GPIO.HIGH)
+  elif input == 'right':
+    GPIO.output(MOTOR_RIGHT, GPIO.HIGH)
+  elif input == 'stop':
+    #turns motors off
+    GPIO.output(MOTOR_RIGHT, GPIO.LOW)
+    GPIO.output(MOTOR_RIGHT, GPIO.LOW)
+    GPIO.cleanup()
+    break
