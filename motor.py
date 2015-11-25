e0b300704f4e636f0afe6c41b28448a606ac6c95
Matan Kotler-Berkowitz <205matan@gmail.com>
2015-11-25 21:27:08 +0000

    Implement event loop with queues

--- motor.py
+++ motor.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import speech_recognition as sr
+from Queue import Queue
 
 #set mode for identifying pins
 GPIO.setmode(GPIO.BOARD)
@@ -8,26 +9,28 @@ GPIO.setmode(GPIO.BOARD)
 MOTOR_RIGHT = 16
 MOTOR_LEFT = 18
 
-command = ''
+q = Queue()
 
 while True:
-  if command == 'left':
-    print('Moving left...')
-    GPIO.output(MOTOR_RIGHT, GPIO.HIGH)
-    GPIO.output(MOTOR_LEFT, GPIO.LOW)
-  elif command == 'right':
-    print('Moving right...')
-    GPIO.output(MOTOR_LEFT, GPIO.HIGH)
-    GPIO.output(MOTOR_RIGHT, GPIO.LOW)
-  elif command == 'forward':
-    print('Moving forward...')
-    GPIO.output(MOTOR_RIGHT, GPIO.HIGH)
-    GPIO.output(MOTOR_LEFT, GPIO.HIGH)
-  elif command == 'stop':
-    print('Stopping motors..')
-    #turns motors off
-    GPIO.output(MOTOR_RIGHT, GPIO.LOW)
-    GPIO.output(MOTOR_LEFT, GPIO.LOW)
-    GPIO.cleanup()
-    print('Motors stopped')
-    break
+  command = q.get()
+  if not command == '':
+    if command == 'left':
+      print('Moving left...')
+      GPIO.output(MOTOR_RIGHT, GPIO.HIGH)
+      GPIO.output(MOTOR_LEFT, GPIO.LOW)
+    elif command == 'right':
+      print('Moving right...')
+      GPIO.output(MOTOR_LEFT, GPIO.HIGH)
+      GPIO.output(MOTOR_RIGHT, GPIO.LOW)
+    elif command == 'forward':
+      print('Moving forward...')
+      GPIO.output(MOTOR_RIGHT, GPIO.HIGH)
+      GPIO.output(MOTOR_LEFT, GPIO.HIGH)
+    elif command == 'stop':
+      print('Stopping motors..')
+      #turns motors off
+      GPIO.output(MOTOR_RIGHT, GPIO.LOW)
+      GPIO.output(MOTOR_LEFT, GPIO.LOW)
+      GPIO.cleanup()
+      print('Motors stopped')
+      break
