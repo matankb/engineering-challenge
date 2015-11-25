793e62cf8dee0052cff4a46d2db3ed9449ead169
Matan Kotler-Berkowitz <205matan@gmail.com>
2015-11-25 11:31:04 -0500

    Add debugging messages

--- motor.py
+++ motor.py
@@ -11,17 +11,22 @@ MOTOR_LEFT = 18
 while True:
   input = input('Direction:') #Will be subsituted for voice control
   if input == 'left':
+    print('Moving left...')
     GPIO.output(MOTOR_LEFT, GPIO.HIGH)
     GPIO.output(MOTOR_RIGHT, GPIO.LOW)
   elif input == 'right':
+    print('Moving right...')
     GPIO.output(MOTOR_RIGHT, GPIO.HIGH)
     GPIO.output(MOTOR_LEFT, GPIO.LOW)
   elif input == 'forward':
+    print('Moving forward...')
     GPIO.output(MOTOR_RIGHT, GPIO.HIGH)
     GPIO.output(MOTOR_LEFT, GPIO.HIGH)
   elif input == 'stop':
+    print('Stopping motors..')
     #turns motors off
     GPIO.output(MOTOR_RIGHT, GPIO.LOW)
     GPIO.output(MOTOR_RIGHT, GPIO.LOW)
     GPIO.cleanup()
+    print('Motors stopped')
     break
