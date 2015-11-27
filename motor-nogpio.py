e7387377538ce0a180c847fc5e040b09982f2977
Matan Kotler-Berkowitz <205matan@gmail.com>
2015-11-27 13:25:50 -0500

    Create no-GPIO python program

---
+++ motor-nogpio.py
@@ -0,0 +1,34 @@
+import speech_recognition as sr
+from Queue import Queue
+
+event_queue = Queue()
+
+rec = sr.Recognizer()
+mic = sr.Microphone()
+
+def phrase_heard(rec, audio):
+  try:
+    event_queue.put(rec.recognize_google(audio))
+  except sr.UnknownValueError:
+    print('Error: could not recognize speech')
+  except sr.RequestError as e:
+    print('Error: could not complete request; {0}'.format(e))
+
+with mic as source:
+  #calibrates for background noise
+  rec.adjust_for_ambient_noise(source)
+
+rec.listen_in_background(mic, phrase_heard)
+
+while True:
+  command = event_queue.get()
+
+  if command == 'left':
+    print('Moving left...')
+  elif command == 'right':
+    print('Moving right...')
+  elif command == 'forward':
+    print('Moving forward...')
+  elif command == 'stop':
+    print('Stopping motors..')
+    print('Motors stopped')
