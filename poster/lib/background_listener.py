8ca7272c69490460ba8dbe192bca14d0d91753e1
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-02-04 17:46:04 -0500

    Add background_listener.py to poster

---
+++ poster/lib/background_listener.py
@@ -0,0 +1,20 @@
+import speech_recognition as sr
+
+rec = sr.Recognizer()
+mic = sr.Microphone()
+
+with mic as source:
+  #calibrates for background noise
+  rec.adjust_for_ambient_noise(source)
+
+def start_listening(onsuccess, onfailure):
+
+    def phrase_heard(rec, audio):
+        try:
+            onsuccess(rec.recognize_google(audio))
+        except sr.UnknownValueError:
+            onfailure('UnknownValueError')
+        except sr.RequestError:
+            onfailure('RequestError')
+
+    rec.listen_in_background(mic, phrase_heard)
